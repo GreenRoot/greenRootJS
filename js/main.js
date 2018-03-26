03750c273edd24795c91a14fbf4eb77418550cb2
AlxGreen <1.ooo.ooo@bk.ru>
2018-03-26 23:14:56 +0300

    add market

---
+++ js/main.js
@@ -0,0 +1,19 @@
+;
+var budget = +prompt("Какой у Вас бюджет?");
+var shopName = prompt("Введите название магазина");
+var open = confirm("Магазин ещё открыт?");
+var employers = {
+	Director: "Наталья",
+	Seller: "Олег",
+	Cashier: "Михаил"
+};
+var mainList = {
+	budget: budget,
+	shopName: shopName,
+	shopGoods: [prompt("Введите название первого продукта"), prompt("Введите название второго продукта"), prompt("Введите название третьего продукта")],
+	employers: employers,
+	open: open,
+};
+var budgetForTheDay = budget/30;
+console.log(mainList);
+alert("Ваш бюджет на день $"+ budgetForTheDay);
