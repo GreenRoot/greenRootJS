a0b52340bf188179643f6c4bdbad387a5b04ca35
AlxGreen <1.ooo.ooo@bk.ru>
2018-03-29 00:34:17 +0300

    update loginAdmin.js

--- js/loginAdmin.js
+++ js/loginAdmin.js
@@ -1,7 +1,19 @@
-var login = prompt('Ваш логин', 'kek');
-
+var login 		= prompt('Ваш логин', 'kek');
+ 
 	if (login == 'admin') {
-		alert('Добро пожаловать Админ')
-	} else {
+		var password 	= prompt('Введите пароль', '********');
+		if (password == '123') {
+			alert('Добро пожаловать, ' + login)
+		} else if ( password == null) {
+			alert('Вход отменён!')
+		} else {
+			alert('Неверный пароль!')
+		}
+
+	} 
+	else if (login == null) {
+		alert('Вход отменён')
+	} 
+	else {
 		alert('Вас нет в базе данных')
 	}
