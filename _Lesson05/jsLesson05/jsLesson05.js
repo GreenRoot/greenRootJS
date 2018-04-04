efabb7ca00c3666ad1cb634f184c81f307ef5258
AlxGreen <1.ooo.ooo@bk.ru>
2018-04-04 08:38:24 +0300

    Added Lesson05

---
+++ _Lesson05/jsLesson05/jsLesson05.js
@@ -0,0 +1,7 @@
+let menuItem = document.querySelectorAll(".menu-item");
+let column = document.getElementsByClassName("column");
+let adv = document.querySelectorAll(".adv");
+
+
+
+document.body.column.removeChild(adv[0]);
