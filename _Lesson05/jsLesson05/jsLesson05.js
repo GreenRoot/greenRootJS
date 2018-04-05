a470d236b96ce31f420b84a4f5a1e1e0eaff2044
AlxGreen <1.ooo.ooo@bk.ru>
2018-04-05 07:21:34 +0300

    Lesson05 has been updated

--- _Lesson05/jsLesson05/jsLesson05.js
+++ _Lesson05/jsLesson05/jsLesson05.js
@@ -1,7 +1,16 @@
+let body = document.getElementsByTagName("body");
 let menuItem = document.querySelectorAll(".menu-item");
-let column = document.getElementsByClassName("column");
-let adv = document.querySelectorAll(".adv");
+let title = document.querySelectorAll(".title")
+let adv = document.querySelector(".adv");
 
 
+menuItem[0].style.order = "1";
+menuItem[1].style.order = "3";
+menuItem[2].style.order = "2";
+menuItem[3].style.order = "4";
+body[0].style.background = "url(img/apple_true.jpg) center no-repeat";
+title[0].textContent = "Мы продаем только подлинную технику Apple";
+adv.remove();
 
-document.body.column.removeChild(adv[0]);
+let answer = setTimeout("prompt('Как Вы относитесь к технике Apple?')", 2000);
+console.log(answer);
