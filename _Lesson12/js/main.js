870658249ff0e91b64802665093d94ae81b26cbd
AlxGreen <1.ooo.ooo@bk.ru>
2018-04-15 22:56:48 +0300

    Lesson12 has been updated

--- _Lesson12/js/main.js
+++ _Lesson12/js/main.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
-
+$(function() {
+	//Нажание на кнопку "Выбрать тур"
 	$(".main_btna").on("click", function() {
 		$(".overlay").fadeIn("slow");
 		$(".modal").slideToggle("slow");
 	});
-
+	//Нажание на кнопку "Получить консультацию"
 	$(".main_btn").on("click", function() {
 		$(".overlay").fadeIn("slow");
 		$(".modal").slideToggle("slow");
 	});
-
+	//Нажание на кнопку "Расписание туров"
 	$('a[href="#sheldure"]').on("click", function() {
 		$(".overlay").fadeIn("slow");
 		$(".modal").slideToggle("slow");
 	});
-
+	//Нажание на простраство вне модального окна
 	$(".overlay").on("click", function() {
 		$(".overlay").fadeOut("slow");
 		$(".modal").slideToggle("slow");
 	})
-
+	//Нажание на кнопку закрытия модального окна
 	$(".close").on("click", function() {
 		$(".overlay").fadeOut("slow");
 		$(".modal").slideToggle("slow");
