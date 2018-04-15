3a0a9c5082b8c56eab8731a3fbb047212f673910
AlxGreen <1.ooo.ooo@bk.ru>
2018-04-15 23:08:26 +0300

    Lesson12 has been updated

--- _Lesson12/js/main.js
+++ _Lesson12/js/main.js
@@ -1,28 +1,33 @@
 $(function() {
-	//Нажание на кнопку "Выбрать тур"
-	$(".main_btna").on("click", function() {
+
+	function openModal() {
 		$(".overlay").fadeIn("slow");
 		$(".modal").slideToggle("slow");
+	};
+
+	function closeModal() {
+		$(".overlay").fadeOut("slow");
+		$(".modal").slideToggle("slow");
+	};
+
+	//Нажание на кнопку "Выбрать тур"
+	$(".main_btna").on("click", function() {
+		openModal();
 	});
 	//Нажание на кнопку "Получить консультацию"
 	$(".main_btn").on("click", function() {
-		$(".overlay").fadeIn("slow");
-		$(".modal").slideToggle("slow");
+		openModal();
 	});
 	//Нажание на кнопку "Расписание туров"
 	$('a[href="#sheldure"]').on("click", function() {
-		$(".overlay").fadeIn("slow");
-		$(".modal").slideToggle("slow");
+		openModal();
 	});
 	//Нажание на простраство вне модального окна
 	$(".overlay").on("click", function() {
-		$(".overlay").fadeOut("slow");
-		$(".modal").slideToggle("slow");
+		closeModal();
 	})
 	//Нажание на кнопку закрытия модального окна
 	$(".close").on("click", function() {
-		$(".overlay").fadeOut("slow");
-		$(".modal").slideToggle("slow");
+		closeModal();
 	})
 });
-
